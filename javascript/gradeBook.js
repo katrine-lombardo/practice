78bf87b368276e0928086a39974d0da692faaad7
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-10 17:45:11 -0500

    Added solution to challenge gradeBook.js

--- javascript/gradeBook.js
+++ javascript/gradeBook.js
@@ -10,15 +10,36 @@
 // Tested values are all between 0 and 100. Theres is no need to check for
 // negative values or values greater than 100.
 
+// STRATEGY
+// Map out scores
+// Add all nums together
+// Divide sum by 3
+// Return solution
+
 function getGrade(s1, s2, s3) {
-  // Code here
+  const letterScores = {
+    A: [90, 100],
+    B: [80, 90],
+    C: [70, 80],
+    D: [60, 70],
+    F: [0, 60],
+  };
+
+  let avg = (s1 + s2 + s3) / 3;
+
+  for (let grade in letterScores) {
+    let range = letterScores[grade];
+    if (avg >= range[0] && avg <= range[1]) {
+      return grade;
+    }
+  }
 }
 
 let example1 = getGrade(95, 90, 93);
 console.log(example1);
 
-let example2 = getGrade(100, 85, 96);
+let example2 = getGrade(70, 70, 100);
 console.log(example2);
 
-let example3 = getGrade(92, 93, 94);
+let example3 = getGrade(75, 70, 79);
 console.log(example3);
