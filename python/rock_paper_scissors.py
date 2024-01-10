c6c242757e693782301befe5e0ca30631572754c
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-10 16:33:09 -0500

    Added solution to challenge rock_paper_scissors

--- python/rock_paper_scissors.py
+++ python/rock_paper_scissors.py
@@ -7,9 +7,20 @@
 # "scissors", "rock" --> "Player 2 won!"
 # "paper", "paper" --> "Draw!"
 
+# STRATEGY
+# Map out what beats what (dict? Or switch case? Or if/elif chain?)
+# Return the result based on values p1 and p2 according to the map
+
 
 def rps(p1, p2):
-    pass
+    winner_combos = {"scissors": "paper", "paper": "rock", "rock": "scissors"}
+    if winner_combos.get(p1) == p2:
+        return "Player 1 won!"
+    elif winner_combos.get(p2) == p1:
+        return "Player 2 won!"
+    else:
+        return "Draw!"
+
 
 print(rps("scissors", "paper"))
 print(rps("scissors", "rock"))
