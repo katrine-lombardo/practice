84cdbda2f29f32bd3ba8616ada8e520676aead33
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-10 15:54:26 -0500

    Added first challenge

---
+++ python/break_camelcase.py
@@ -0,0 +1,18 @@
+# Complete the solution so that the function will break up camel casing, using a space between words.
+
+# Example
+# "camelCasing"  =>  "camel Casing"
+# "identifier"   =>  "identifier"
+# ""             =>  ""
+
+# Strategy:
+# Create new_string variable to store output
+# Loop through each letter in word
+# Check if the letter equals its uppercase equivalent
+# If it does not, append the letter to new_string
+# If it does, join a space with the new letter and append this to new_string
+# Return the new word
+# Refactor the solution
+
+def solution(s):
+    pass
