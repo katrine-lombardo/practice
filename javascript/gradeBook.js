d74f4596951557329040de3aaf51b94be6f5831e
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-10 17:02:17 -0500

    Added gradeBook.js challenge

---
+++ javascript/gradeBook.js
@@ -0,0 +1,24 @@
+// Complete the function so that it finds the average of the three
+// scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for
+// negative values or values greater than 100.
+
+function getGrade(s1, s2, s3) {
+  // Code here
+}
+
+let example1 = getGrade(95, 90, 93);
+console.log(example1);
+
+let example2 = getGrade(100, 85, 96);
+console.log(example2);
+
+let example3 = getGrade(92, 93, 94);
+console.log(example3);
