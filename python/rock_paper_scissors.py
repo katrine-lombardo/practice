bcb2b327456559a9094655d4ac8e45c47b6047c6
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-10 16:10:45 -0500

    Added challenge rock_paper_scissors

---
+++ python/rock_paper_scissors.py
@@ -0,0 +1,16 @@
+# Rock Paper Scissors
+# Let's play! You have to return which player won! In case of a draw return Draw!.
+
+# Examples(Input1, Input2 --> Output):
+
+# "scissors", "paper" --> "Player 1 won!"
+# "scissors", "rock" --> "Player 2 won!"
+# "paper", "paper" --> "Draw!"
+
+
+def rps(p1, p2):
+    pass
+
+print(rps("scissors", "paper"))
+print(rps("scissors", "rock"))
+print(rps("paper", "paper"))
