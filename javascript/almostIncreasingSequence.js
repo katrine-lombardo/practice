81bbcd19dd42fb570b35f63d2f097ff98c0f9b0f
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-12 15:31:08 -0500

    Added refactored solution to classed almostIncreasing to improve BigO time complexity from O(n^2) to O(n)

--- javascript/almostIncreasingSequence.js
+++ javascript/almostIncreasingSequence.js
@@ -50,3 +50,41 @@ console.log(sample2);
 
 const sample3 = solution([1, 2, 1, 2]); // --> false
 console.log(sample3);
+
+// REFACTORED
+// Improve big-o time complexity from O(n^2) to O(n)
+
+// STRATEGY
+// Iterate through the sequence once
+// Keep track of the count of removed elements, and
+// If any pair's first item is greater than the second, increment it by 1
+//     If at any point removedCount exceeds 1, break out and return false
+//     If removing the current element helps, update it 
+
+function refactoredSolution(sequence) {
+  let removedCount = 0;
+
+  for (let i = 0; i < sequence.length - 1; i++) {
+    if (sequence[i] >= sequence[i + 1]) {
+      removedCount++;
+
+      if (removedCount > 1) {
+        return false;
+      }
+
+      if (i > 0 && sequence[i - 1] >= sequence[i + 1]) {
+        sequence[i + 1] = sequence[i];
+      }
+    }
+  }
+  return true;
+}
+
+const sample4 = refactoredSolution([1, 3, 2, 1]); // --> false
+console.log(sample4);
+
+const sample5 = refactoredSolution([1, 3, 2]); // --> true
+console.log(sample5);
+
+const sample6 = refactoredSolution([1, 2, 1, 2]); // --> false
+console.log(sample6);
