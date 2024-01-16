b381b45923e4131bef86376bc7c57463cc409565
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-16 15:20:21 -0500

    Added challenge allLongestStrings

---
+++ javascript/allLongestStrings.js
@@ -0,0 +1,18 @@
+/* Given an array of strings, return another array containing all of its longest strings.
+
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+solution(inputArray) = ["aba", "vcd", "aba"].
+*/
+
+function solution(inputArray) {}
+
+const example1 = solution(["aba", "aa", "ad", "vcd", "aba"]);
+console.log(`${example1} should be ["aba", "vcd", "aba"]`);
+
+const example2 = solution(["aa"]);
+console.log(`${example2} should be ["aa"]`);
+
+const example3 = solution(["abc", "eeee", "abcd", "dcd"]);
+console.log(`${example3} should be ["eeee", "abcd"]`);
