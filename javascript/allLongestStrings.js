e034e6641fbc85e830c2eb01794bf81301a6c56e
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-16 15:50:46 -0500

    Added solution to challenge allLongestStrings

--- javascript/allLongestStrings.js
+++ javascript/allLongestStrings.js
@@ -6,7 +6,24 @@ For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
 solution(inputArray) = ["aba", "vcd", "aba"].
 */
 
-function solution(inputArray) {}
+// STRATEGY
+// Input: Array of strings
+// Output: Array of string(s)
+// Find item in the array with the max length
+// Return a filtered list containing only items of that length
+
+function solution(inputArray) {
+  longestWord = "";
+  for (let word of inputArray) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  const longestWordArray = inputArray.filter(
+    (word) => word.length === longestWord.length
+  );
+  return longestWordArray;
+}
 
 const example1 = solution(["aba", "aa", "ad", "vcd", "aba"]);
 console.log(`${example1} should be ["aba", "vcd", "aba"]`);
