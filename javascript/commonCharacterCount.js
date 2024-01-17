ef27f34361cd94a562804117c16ef5f9d329e39b
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-17 17:40:23 -0500

    Added challenge commonCharacterCount

---
+++ javascript/commonCharacterCount.js
@@ -0,0 +1,17 @@
+// Given two strings, find the number of common characters between them.
+
+// Example:
+// For s1 = "aabcc" and s2 = "adcaa", the output should be
+// solution(s1, s2) = 3.
+// Strings have 3 common characters - 2 "a"s and 1 "c".
+
+function solution(s1, s2) {}
+
+const example1 = solution("aabcc", "adcaa");
+console.log(`${example1} should be 3`);
+
+const example2 = solution("zzzz", "zzzzzzz");
+console.log(`${example2} should be 4`);
+
+const example3 = solution("abca", "xyzbac");
+console.log(`${example3} should be 3`);
