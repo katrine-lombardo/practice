e7995316286201b97390117b945c47a5cc1e25f6
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-18 12:51:27 -0500

    Added strategy, notes, and test cases to isLucky

--- javascript/isLucky.js
+++ javascript/isLucky.js
@@ -11,4 +11,26 @@ For n = 239017, the output should be
 solution(n) = false.
 */
 
+// STRATEGY
+// Input: int
+// Output: bool
+// Notes: Edge cases could include 1-digit integers, or odd-numbered length of
+// integers (though codeSignal confirms that it will always be even).
+// Logic:
+// I can convert the int to a string to produce the two slices.
+// I could alternatively use concatenate and the index number at half the length
+// of the string.
+// To sum each number in the int individually, I can loop through each half one
+// time and store the sum in a counter variable.
+// The final test is if the sums equal one another.
+
 function solution(n) {}
+
+const example1 = solution(1230);
+console.log(`${example1} should = true`);
+
+const example2 = solution(239017);
+console.log(`${example2} should = false`);
+
+const example3 = solution(134008);
+console.log(`${example3} should = true`);
