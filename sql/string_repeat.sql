efb20570f506dbbbc65f33eb8dfbabff02b9f993
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-22 16:34:43 -0500

    Added SQL challenge db

--- sql/string_repeat.sql
+++ sql/string_repeat.sql
@@ -1,8 +1,20 @@
--- Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+-- Write a function that accepts an integer n and a string s as parameters, and
+-- returns a string of s repeated exactly n times.
 
 -- Examples (input -> output)
 -- 6, "I"     -> "IIIIII"
 -- 5, "Hello" -> "HelloHelloHelloHelloHello"
 
 --# write your SQL statement here: you are given a table 'repeatstr' with columns 'n' and 's', return a table with all columns and your result in a column named 'res'.
+
+CREATE TABLE IF NOT EXISTS repeatstr (
+    id INTEGER PRIMARY KEY,
+    n INT,
+    s VARCHAR(255)
+);
+
+INSERT INTO repeatstr (n, s) VALUES
+    (3, "*"),
+    (5, "#"),
+    (2, "ha");
 
