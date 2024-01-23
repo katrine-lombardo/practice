60b217c0103b96468a10ed1dc9b884c5b3012e35
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-23 10:22:52 -0500

    [hackerrank]: Added notes and strategy to finding_the_percentage

--- python/finding_the_percentage.py
+++ python/finding_the_percentage.py
@@ -1,5 +1,7 @@
 """
-The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+The provided code stub will read in a dictionary containing key/value pairs of
+name: [marks] for a list of students. Print the average of the marks array for
+the student name provided, showing 2 places after the decimal.
 
 Sample 1 Input:
     3
@@ -10,12 +12,25 @@ Sample 1 Input:
 Sample 2 Output: 56.00
 
 Sample 1 Input:
-2
-Harsh 25 26.5 28
-Anurag 26 28 30
-Harsh
+    2
+    Harsh 25 26.5 28
+    Anurag 26 28 30
+    Harsh
 Sample 1 Output: 26.50
 """
+
+# NOTES
+# Input: Multiple lines - int, then x numbers of strings (including ints) then a
+# string corresponding to a particular name.
+# Output: Float
+# Strategy: Figure out how to isolate the items on each line to handle the logic
+# on the individual pieces. The first line int tells me how many rows of data to
+# expect, so that should help me to isolate that data.
+# Based on the input type, it seems like a good structure for the lines of data
+# would be a dict, with k = name and v = list of grades.
+# Once the dict is populated I can lookup a particular name in the dict, and
+# manually calculate the average of the items in the list.
+
 if __name__ == "__main__":
     n = int(input())
     student_marks = {}
