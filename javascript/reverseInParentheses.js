8eead288f3cd90a525a9880a07fa6f1fef5a1967
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-01-25 13:07:56 -0500

    [codesignal]: Added solution for reverseInParentheses with O(n) time complexity

--- javascript/reverseInParentheses.js
+++ javascript/reverseInParentheses.js
@@ -27,7 +27,18 @@ solution(inputString) = "foobazrabblim".
 // Logic: I need to isolate each section of the string, and work from the
 // inside-out to reverse in that specific order, and concatenate to the eventual
 // result string.
-// Strategy:
+
+// Strategy that worked:
+// 1. Create an empty stack to keep track of the idx location of parentheses.
+// 2. Create an empty str to store the final output.
+// 3. Iterate through each character of the string to find those parentheses.
+// 4. If the char is an open parenthesis, save the current state to the stack
+//    and reset result to an empty string.
+// 5. If the character isn't a parenthesis, add that character to the result.
+// 6. If the char is a close parenthesis, pop the current state from the stack
+//    and add the reversed characters to the result variable.
+
+// (old) Strategy that did not work:
 // 1. Determine the number of "parenthesed" strings (for testing).
 // 2. Get the index location for each open-parenthesis and store in an array so
 //    I know where to insert the substring.
@@ -37,16 +48,32 @@ solution(inputString) = "foobazrabblim".
 // 5. Loop through the original array to replace substrings corresponding to the
 //    index location from step 2.
 
-function solution(inputString) {}
+function solution(inputString) {
+  let stack = [];
+  let result = "";
+
+  for (let char of inputString) {
+    if (char === "(") {
+      stack.push(result);
+      result = "";
+    } else if (char === ")") {
+      result = stack.pop() + result.split("").reverse().join("");
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+}
 
 inputString0 = solution("(bar)");
-print(`${inputString0} should = "rab"`);
+console.log(`${inputString0} should = "rab"`);
 
 inputString1 = solution("foo(bar)baz");
-print(`${inputString1} should =  "foorabbaz"`);
+console.log(`${inputString1} should = "foorabbaz"`);
 
 inputString2 = solution("foo(bar)baz(blim)");
-print(`${inputString2} should = "foorabbazmilb"`);
+console.log(`${inputString2} should = "foorabbazmilb"`);
 
 inputString3 = solution("foo(bar(baz))blim");
-print(`${inputString3} should = "foobazrabblim"`);
+console.log(`${inputString3} should = "foobazrabblim"`);
