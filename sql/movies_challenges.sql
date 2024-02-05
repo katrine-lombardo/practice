a52b12824b6f96d176daa21e3d95842d8d5ba0ed
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-05 12:37:09 -0500

    [SQL/Movies]: Completed challenge 14

--- sql/movies_challenges.sql
+++ sql/movies_challenges.sql
@@ -94,3 +94,11 @@ WHERE ord=1
 GROUP BY actorid, name
 HAVING COUNT(ord) >= 15
 ORDER BY name;
+
+-- 14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+SELECT title, COUNT(actorid) FROM movie
+JOIN casting ON (movie.id=movieid)
+JOIN actor ON (actor.id=actorid)
+WHERE yr=1978
+GROUP BY title
+ORDER BY COUNT(actorid) DESC, title;
