1e912311780b4ae9c910497bc0d410441e560ff9
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-15 16:57:10 -0500

    [SQL]: Practicing SQL and Postgres from pgexercises.com

---
+++ sql/exercises_challenges.sql
@@ -0,0 +1,52 @@
+-- Simple SQL Queries
+-- 01. Retrieve everything from a table
+-- How can you retrieve all the information from the cd.facilities table?
+SELECT * FROM cd.facilities;
+
+-- 02. Retrieve specific columns from a table
+--     You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?
+SELECT name, membercost FROM cd.facilities;
+
+-- 03. Control which rows are retrieved
+-- How can you produce a list of facilities that charge a fee to members?
+SELECT * FROM cd.facilities
+WHERE membercost > 0;
+
+-- 04. Control which rows are retrieved - part 2
+--     How can you produce a list of facilities that charge a fee to members,
+--     and that fee is less than 1/50th of the monthly maintenance cost? Return the facid, facility name, member cost, and monthly maintenance of the
+--     facilities in question.
+SELECT facid, name, membercost, monthlymaintenance
+FROM cd.facilities
+WHERE membercost < (monthlymaintenance/50)
+	AND membercost > 0;
+
+-- 05. Basic string searches
+-- How can you produce a list of all facilities with the word 'Tennis' in their name?
+SELECT * FROM cd.facilities
+WHERE name LIKE '%Tennis%';
+
+-- 06. Matching against multiple possible values
+-- How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the OR operator.
+SELECT * FROM cd.facilities
+WHERE facid in (1,5);
+
+-- 07. Classify results into buckets
+-- How can you produce a list of facilities, with each labelled as 'cheap' or 'expensive' depending on if their monthly maintenance cost is more than $100? Return the name and monthly maintenance of the facilities in question.
+SELECT name,
+	CASE WHEN (monthlymaintenance > 100)
+		THEN 'expensive'
+		ELSE 'cheap'
+		END
+	AS cost
+FROM cd.facilities;
+
+-- 08. Working with dates
+
+-- 09. Removing duplicates, and ordering results
+
+-- 10. Combining results from multiple queries
+
+-- 11. Simple aggregation
+
+-- 12. More aggregation
