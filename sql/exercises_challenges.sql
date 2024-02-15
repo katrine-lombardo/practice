01aa7a4c7518984b1a10b800713aca0181a6b3e7
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-15 17:19:56 -0500

    [SQL]: Solved problem 12: More aggregation

--- sql/exercises_challenges.sql
+++ sql/exercises_challenges.sql
@@ -64,3 +64,8 @@ SELECT name FROM cd.facilities;
 SELECT MAX(joindate) AS latest FROM cd.members;
 
 -- 12. More aggregation
+-- You'd like to get the first and last name of the last member(s) who signed up - not just the date. How can you do that?
+SELECT firstname, surname, joindate FROM cd.members
+WHERE joindate = (
+    SELECT MAX(joindate) FROM cd.members
+);
