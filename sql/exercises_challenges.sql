95a4cf847aff42dfe82561f9ae26de81126c8102
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-15 17:02:57 -0500

    [SQL]: Solved problem 08

--- sql/exercises_challenges.sql
+++ sql/exercises_challenges.sql
@@ -42,6 +42,10 @@ SELECT name,
 FROM cd.facilities;
 
 -- 08. Working with dates
+-- How can you produce a list of members who joined after the start of September 2012? Return the memid, surname, firstname, and joindate of the members in question.
+SELECT memid, surname, firstname, joindate
+FROM cd.members
+WHERE joindate > '2012-09-01';
 
 -- 09. Removing duplicates, and ordering results
 
