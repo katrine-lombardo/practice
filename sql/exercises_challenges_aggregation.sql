88cc7ae09af2fe91dc4fbc08c41f0151a762f4cc
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-20 14:10:41 -0500

    [SQL]: Added Aggregation challenges

---
+++ sql/exercises_challenges_aggregation.sql
@@ -0,0 +1,65 @@
+-- 01. Count the number of facilities
+
+
+-- 02. Count the number of expensive facilities
+
+
+-- 03. Count the number of recommendations each member makes.
+
+
+-- 04. List the total slots booked per facility
+
+
+-- 05. List the total slots booked per facility in a given month
+
+
+-- 06. List the total slots booked per facility per month
+
+
+-- 07. Find the count of members who have made at least one booking
+
+
+-- 08. List facilities with more than 1000 slots booked
+
+
+-- 09. Find the total revenue of each facility
+
+
+-- 10. Find facilities with a total revenue less than 1000
+
+
+-- 11. Output the facility id that has the highest number of slots booked
+
+
+-- 12. List the total slots booked per facility per month, part 2
+
+
+-- 13. List the total hours booked per named facility
+
+
+-- 14. List each member's first booking after September 1st 2012
+
+
+-- 15. Produce a list of member names, with each row containing the total member
+--     count
+
+
+-- 16. Produce a numbered list of members
+
+
+-- 17. Output the facility id that has the highest number of slots booked, again
+
+
+-- 18. Rank members by (rounded) hours used
+
+
+-- 19. Find the top three revenue generating facilities
+
+
+-- 20. Classify facilities by value
+
+
+-- 21. Calculate the payback time for each facility
+
+
+-- 22. Calculate a rolling average of total revenue
