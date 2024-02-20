8c169cc503ee5b1e8c0e4534ceed5547dc00d4de
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-20 14:30:13 -0500

    [SQL]: Solved problem 05: Sum between dates

--- sql/exercises_challenges_aggregation.sql
+++ sql/exercises_challenges_aggregation.sql
@@ -24,8 +24,12 @@ GROUP BY facid
 ORDER BY facid;
 
 -- 05. List the total slots booked per facility in a given month
-
-
+-- Produce a list of the total number of slots booked per facility in the month of September 2012. Produce an output table consisting of facility id and slots, sorted by the number of slots.
+SELECT facid, SUM(slots) AS "Total Slots"
+FROM cd.bookings
+WHERE DATE(starttime) BETWEEN '2012-09-01' AND '2012-09-30'
+GROUP BY facid
+ORDER BY SUM(slots);
 
 -- 06. List the total slots booked per facility per month
 
