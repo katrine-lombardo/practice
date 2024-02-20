33caae41d7aad7f4e46010056dfab3e250eb91fd
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-20 13:57:59 -0500

    [SQL]: Solved problem 06: Update a row based on content of another row

--- sql/exercises_challenges_updates.sql
+++ sql/exercises_challenges_updates.sql
@@ -33,8 +33,12 @@ WHERE name LIKE '%Tennis Court%';
 
 
 -- 06. Update a row based on the contents of another row
-
-
+-- We want to alter the price of the second tennis court so that it costs 10% more than the first one. Try to do this without using constant values for the prices, so that we can reuse the statement if we want to.
+UPDATE cd.facilities
+SET
+    membercost=(SELECT membercost*1.1 FROM cd.facilities WHERE name='Tennis Court 1'),
+    guestcost=(SELECT guestcost*1.1 FROM cd.facilities WHERE name='Tennis Court 1')
+WHERE name='Tennis Court 2';
 
 -- 07. Delete all bookings
 
