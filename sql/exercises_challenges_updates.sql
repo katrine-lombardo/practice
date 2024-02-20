a8e7a194a1662c80f7b4ddca855b59d8ca977d38
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-20 12:48:26 -0500

    [SQL]: Solved problem 01: Insert data into table

--- sql/exercises_challenges_updates.sql
+++ sql/exercises_challenges_updates.sql
@@ -1,6 +1,8 @@
 -- 01. Insert some data into a table
-
-
+-- The club is adding a new facility - a spa. We need to add it into the facilities table. Use the following values:
+-- facid: 9, Name: 'Spa', membercost: 20, guestcost: 30, initialoutlay: 100000, monthlymaintenance: 800.
+INSERT INTO cd.facilities (facid, name, membercost, guestcost, initialoutlay, monthlymaintenance) VALUES
+(9, 'Spa', 20, 30, 100000, 800);
 
 -- 02. Insert multiple rows of data into a table
 
