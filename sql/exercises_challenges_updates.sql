f98df9bad119012c6a1cdf9b0a336ef13ba17b0f
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-20 12:43:53 -0500

    [SQL]: Added Modifying Data challenges

---
+++ sql/exercises_challenges_updates.sql
@@ -0,0 +1,33 @@
+-- 01. Insert some data into a table
+
+
+
+-- 02. Insert multiple rows of data into a table
+
+
+
+-- 03. Insert calculated data into a table
+
+
+
+-- 04. Update some existing data
+
+
+
+-- 05. Update multiple rows and columns at the same time
+
+
+
+-- 06. Update a row based on the contents of another row
+
+
+
+-- 07. Delete all bookings
+
+
+
+-- 08. Delete a member from the cd.members table
+
+
+
+-- 09. Delete based on a subquery
