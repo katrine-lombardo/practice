c1177b8563058d5936b29920b2e17916c331c4b1
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-20 14:07:37 -0500

    [SQL]: Solved problem 09: Delete rows based on data in another table

--- sql/exercises_challenges_updates.sql
+++ sql/exercises_challenges_updates.sql
@@ -49,3 +49,6 @@ DELETE FROM cd.bookings;
 DELETE FROM cd.members WHERE memid=37;
 
 -- 09. Delete based on a subquery
+-- In our previous exercises, we deleted a specific member who had never made a booking. How can we make that more general, to delete all members who have never made a booking?
+DELETE FROM cd.members
+WHERE memid NOT IN (SELECT memid FROM cd.bookings);
