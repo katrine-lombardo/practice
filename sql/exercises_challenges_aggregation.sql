bb6f7af7d444fd0923ab7db9064fca26f4b2efe1
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-02-20 14:24:06 -0500

    [SQL]: Solved problem 04: Sum of slots

--- sql/exercises_challenges_aggregation.sql
+++ sql/exercises_challenges_aggregation.sql
@@ -17,8 +17,11 @@ ORDER BY recommendedby;
 
 
 -- 04. List the total slots booked per facility
-
-
+-- Produce a list of the total number of slots booked per facility. For now, just produce an output table consisting of facility id and slots, sorted by facility id.
+SELECT facid, SUM(slots) AS "Total Slots"
+FROM cd.bookings
+GROUP BY facid
+ORDER BY facid;
 
 -- 05. List the total slots booked per facility in a given month
 
