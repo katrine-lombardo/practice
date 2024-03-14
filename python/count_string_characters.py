816e7a7bce4cc470b3c539a9155caec490fc94a6
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-14 18:18:07 -0400

    [Python]: Added refactored solution for Codewars challenge

--- python/count_string_characters.py
+++ python/count_string_characters.py
@@ -6,7 +6,6 @@
 
 
 def count(s):
-    # The function code should be here
     freq_dict = {}
 
     for char in s:
@@ -16,6 +15,10 @@ def count(s):
             freq_dict[char] = 1
 
     return freq_dict
+
+
+def count_refactored(s):
+    return {char: s.count(char) for char in s}
 
 
 # ---------------------------------- TESTS ----------------------------------
@@ -31,3 +34,16 @@ print(f"{test2} should = {solution2}")
 test3 = count("aa")
 solution3 = "{'a' : 2}"
 print(f"{test3} should = {solution3}")
+
+
+test4 = count_refactored("aba")
+solution4 = "{'a': 2, 'b': 1}"
+print(f"{test4} should = {solution4}")
+
+test5 = count_refactored("")
+solution5 = "{}"
+print(f"{test5} should = {solution5}")
+
+test6 = count_refactored("aa")
+solution6 = "{'a' : 2}"
+print(f"{test6} should = {solution6}")
