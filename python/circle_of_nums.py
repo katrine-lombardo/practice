cad07ae97459453eacdac19d67d17c05da90ade6
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-19 15:10:11 -0400

    [Python]: Added refactored solution to CodeSignal challenge circle of nums

--- python/circle_of_nums.py
+++ python/circle_of_nums.py
@@ -20,6 +20,10 @@ def solution(n, firstNumber):
         return halfway
 
 
+def refactored_solution(n, firstNumber):
+    return (firstNumber + (n / 2)) % n
+
+
 print("-------TESTS-------")
 test1 = solution(10, 2)
 solution1 = 7
@@ -28,6 +32,15 @@ solution2 = 15
 test3 = solution(6, 3)
 solution3 = 0
 if test1 == solution1 and test2 == solution2 and test3 == solution3:
+    print("Yay! All tests passed")
+else:
+    print("Errors")
+
+
+test4 = refactored_solution(10, 2)
+test5 = refactored_solution(18, 6)
+test6 = refactored_solution(6, 3)
+if test4 == solution1 and test5 == solution2 and test6 == solution3:
     print("Yay! All tests passed")
 else:
     print("Errors")
