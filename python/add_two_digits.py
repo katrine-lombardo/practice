3814c79e66d61f0852f5b184d5e2e420dc543bd8
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-03-19 14:18:45 -0400

    [python]: Added codesignal challenge add_two_digits

---
+++ python/add_two_digits.py
@@ -0,0 +1,25 @@
+"""
+You are given a two-digit integer n. Return the sum of its digits.
+
+Example
+
+For n = 29, the output should be
+solution(n) = 11.
+"""
+
+
+def solution(n):
+    stringed_n = str(n)
+    first_digit = int(stringed_n[0:1])
+    second_digit = int(stringed_n[1:])
+    return first_digit + second_digit
+
+
+print("-------TESTS-------")
+test1 = solution(29)
+test2 = solution(48)
+test3 = solution(10)
+if test1 == 11 and test2 == 12 and test3 == 1:
+    print("Yay! All tests passed")
+else:
+    print("Errors")
