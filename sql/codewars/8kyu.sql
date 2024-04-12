c79f0c76e149fcf8effc94e3f67047280a5515fc
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-04-12 14:29:01 -0400

    [SQL]: Solution to challenge 8

--- sql/codewars/8kyu.sql
+++ sql/codewars/8kyu.sql
@@ -40,3 +40,7 @@ ORDER BY age DESC;
 -- 7. Given a demographics table you need to return the same table where all
 --    letters are lowercase in the race column.
 SELECT id, name, birthday, LOWER(race) as race FROM demographics;
+
+-- 8. You have access to a table of monsters. Your task is to turn the numeric
+--    columns (arms, legs) into equivalent hexadecimal values.
+SELECT TO_HEX(legs) AS legs, TO_HEX(arms) AS arms FROM monsters;
