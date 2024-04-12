1b6e38f5085ab1eb5156848b0e2e1899b4caea82
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-04-12 13:44:36 -0400

    [SQL]: Solution to challenge 3

--- sql/codewars/8kyu.sql
+++ sql/codewars/8kyu.sql
@@ -6,3 +6,6 @@ SELECT * FROM participants;
 -- 2. You are working for a local school, and you are responsible for collecting tuition from students. You have a list of all students, some of them have already paid tution and some haven't. Write a select statement to get a list of all students who haven't paid their tuition yet. The list should include all the data available about these students.
 SELECT * FROM students
 WHERE tuition_received = FALSE;
+
+-- 3. Given the table 'decimals' return a table with one column (mod) which is the output of number1 modulus number2.
+SELECT (number1 % number2) as mod FROM decimals;
