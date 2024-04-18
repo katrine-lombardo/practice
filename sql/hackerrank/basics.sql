8d177ad7706ba91fc6cfba5bd7bf3e8925bd4b8b
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-04-18 17:20:17 -0400

    [SQL]: Solution to Hackerrank basics challenge 6

--- sql/hackerrank/basics.sql
+++ sql/hackerrank/basics.sql
@@ -19,3 +19,8 @@ WHERE ID = 1661;
 --    COUNTRYCODE for Japan is JPN.
 SELECT * FROM CITY
 WHERE COUNTRYCODE = 'JPN';
+
+-- 6. Query the names of all the Japanese cities in the CITY table. The
+--    COUNTRYCODE for Japan is JPN.
+SELECT NAME FROM CITY
+WHERE COUNTRYCODE = 'JPN';
