b2c22d69f5c82a5c0661b4d7f198938a542c93f1
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-04-18 17:22:59 -0400

    [SQL]: Solution to Hackerrank basics challenge 8

--- sql/hackerrank/basics.sql
+++ sql/hackerrank/basics.sql
@@ -27,3 +27,9 @@ WHERE COUNTRYCODE = 'JPN';
 
 -- 7. Query a list of CITY and STATE from the STATION table.
 SELECT CITY, STATE FROM STATION;
+
+-- 8. Query a list of CITY names from STATION for cities that have an even ID
+--    number. Print the results in any order, but exclude duplicates from the
+--    answer.
+SELECT DISTINCT CITY FROM STATION
+WHERE ID % 2 = 0;
