39878d8a8dad6868828f06940517e4144a10e339
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-04-18 17:17:39 -0400

    [SQL]: Solution to Hackerrank basics challenge 3

--- sql/hackerrank/basics.sql
+++ sql/hackerrank/basics.sql
@@ -3,7 +3,10 @@
 SELECT * FROM CITY
 WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
 
---2. Query the NAME field for all American cities in the CITY table with
+-- 2. Query the NAME field for all American cities in the CITY table with
 --   populations larger than 120000. The CountryCode for America is USA.
 SELECT NAME FROM CITY
 WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+
+-- 3. Query all columns (attributes) for every row in the CITY table.
+SELECT * FROM CITY;
