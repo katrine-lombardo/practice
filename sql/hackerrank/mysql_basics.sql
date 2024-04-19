44264e3b89462fb162d026d2b9dfcd9ee7a38b5e
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-04-19 11:41:06 -0400

    [SQL]: Solution to Hackerrank basics challenge 13

--- sql/hackerrank/mysql_basics.sql
+++ sql/hackerrank/mysql_basics.sql
@@ -65,3 +65,9 @@ WHERE CITY REGEXP '^[AEIOU]';
 --     STATION. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '[AEIOU]$';
+
+-- 13. Query the list of CITY names from STATION which have vowels (i.e., a, e,
+--     i, o, and u) as both their first and last characters. Your result cannot
+--     contain duplicates.
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[AEIOU]' AND CITY REGEXP '[AEIOU]$';
