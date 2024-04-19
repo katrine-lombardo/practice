38f026706df520144088ddd2e3028ed7c0d94f71
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-04-19 11:54:36 -0400

    [SQL]: Solution to Hackerrank basics challenge 18

--- sql/hackerrank/mysql_basics.sql
+++ sql/hackerrank/mysql_basics.sql
@@ -91,3 +91,11 @@ WHERE CITY REGEXP '^[^AEIOU]' OR CITY REGEXP '[^AEIOU]$';
 --     and do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[^AEIOU]' AND CITY REGEXP '[^AEIOU]$';
+
+-- 18. Query the Name of any student in STUDENTS who scored higher than  Marks.
+--     Order your output by the last three characters of each name. If two or
+--     more students both have names ending in the same last three characters
+--     (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+SELECT Name FROM STUDENTS
+WHERE Marks > 75
+ORDER BY SUBSTRING(Name, -3) ASC, ID ASC;
