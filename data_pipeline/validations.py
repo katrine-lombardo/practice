b58e79a4280d1aaa059f64419c83621877980bc0
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-06-06 10:25:14 -0400

    [ETL]: Research notes on validation scripts and best practices

---
+++ data_pipeline/validations.py
@@ -0,0 +1,12 @@
+"""
+Stages of Validation in ETL
+
+1) EXTRACTION: Validate source data (completeness, accuracy, initial QC)
+2) TRANSFORMATION: Validate transformation rules (validate rules, consistency
+after transformation, null check)
+3) LOAD: Validate target data (completeness, integrity like FKs, reconciliation)
+
+Techniques for Data Validation in ETL: Data type, range, constraint,
+consistency, uniqueness, referential integrity
+
+"""
