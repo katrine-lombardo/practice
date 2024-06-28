936b3a8fc22885e9ece91da507b1c44b81beff80
Katrine Lombardo <katrine.lombardo@gmail.com>
2024-06-28 17:56:55 -0400

    [SQL]: Added solution to challenge 8

--- sql/exercises_challenges_aggregation.sql
+++ sql/exercises_challenges_aggregation.sql
@@ -57,7 +57,13 @@ SELECT COUNT(*) FROM
 
 
 -- 08. List facilities with more than 1000 slots booked
-
+--     Produce a list of facilities with more than 1000 slots booked. Produce an
+--     output table consisting of facility id and slots, sorted by facility id.
+SELECT facid, SUM(slots) AS "Total Slots"
+FROM cd.bookings
+GROUP BY facid
+HAVING SUM(slots) > 1000
+ORDER BY facid;
 
 
 -- 09. Find the total revenue of each facility
